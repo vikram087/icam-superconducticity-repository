b36b3d073d3ba2660a79afaba4d71703e4e4461c
Ubuntu <ubuntu@ip-172-31-30-247.ec2.internal>
2024-10-30 09:37:30 +0000

    finalized matscholar model

--- backend/scripts/annotate_texts.py
+++ backend/scripts/annotate_texts.py
@@ -84,14 +84,17 @@ def perform_analysis(ner_model: NERClassifier, abstract: dict):
     return tags
 
 
+abstract = "© 2016 WILEY-VCH Verlag GmbH & Co. KGaA, Weinheim. There is much current interest in combining superconductivity and spin-orbit coupling in order to induce the topological superconductor phase and associated Majorana-like quasiparticles which hold great promise towards fault-tolerant quantum computing. Experimentally these effects have been combined by the proximity-coupling of super-conducting leads and high spin-orbit materials such as InSb and InAs, or by controlled Cu-doping of topological insu-lators such as Bi2Se3. However, for practical purposes, a single-phase material which intrinsically displays both these effects is highly desirable. Here we demonstrate coexisting superconducting correlations and spin-orbit coupling in molecular-beam-epitaxy-grown thin films of GeTe. The former is evidenced by a precipitous low-temperature drop in the electrical resistivity which is quelled by a magnetic field, and the latter manifests as a weak antilocalisation (WAL) cusp in the magnetotransport. Our studies reveal several other intriguing features such as the presence of two-dimensional rather than bulk transport channels below 2 K, possible signatures of topological superconductivity, and unexpected hysteresis in the magnetotransport. Our work demonstrates GeTe to be a potential host of topological SC and Majorana-like excitations, and to be a versatile platform to develop quantum information device architectures."
+
 def main(args) -> None:
     ner_model: NERClassifier = load("ner")
-    abstracts: dict = get_abstracts(args.size)
+ #   abstracts: dict = get_abstracts(args.size)
 
-    tags = [
-        perform_analysis(ner_model, {abstract: abstracts[abstract]})
-        for abstract in abstracts
-    ]
+#    tags = [
+#        perform_analysis(ner_model, {abstract: abstracts[abstract]})
+#        for abstract in abstracts
+#    ]
+    tags = perform_analysis(ner_model, abstract) 
 
     print(tags)
 
