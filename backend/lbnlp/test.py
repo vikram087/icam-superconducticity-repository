a9595b845f676592ba5c8470aa03f9050c725ca1
Vikram Penumarti <vikram.penumarti@gmail.com>
2024-11-02 02:40:17 -0700

    test

--- backend/lbnlp/test.py
+++ backend/lbnlp/test.py
@@ -66,7 +66,7 @@ def model_selection(model_type):
     return ner_model
 
 
-def get_documents(num):
+def get_documents():
     doc = "© 2016 WILEY-VCH Verlag GmbH & Co. KGaA, Weinheim. There is much current interest in combining superconductivity and spin-orbit coupling in order to induce the topological superconductor phase and associated Majorana-like quasiparticles which hold great promise towards fault-tolerant quantum computing. Experimentally these effects have been combined by the proximity-coupling of super-conducting leads and high spin-orbit materials such as InSb and InAs, or by controlled Cu-doping of topological insu-lators such as Bi2Se3. However, for practical purposes, a single-phase material which intrinsically displays both these effects is highly desirable. Here we demonstrate coexisting superconducting correlations and spin-orbit coupling in molecular-beam-epitaxy-grown thin films of GeTe. The former is evidenced by a precipitous low-temperature drop in the electrical resistivity which is quelled by a magnetic field, and the latter manifests as a weak antilocalisation (WAL) cusp in the magnetotransport. Our studies reveal several other intriguing features such as the presence of two-dimensional rather than bulk transport channels below 2 K, possible signatures of topological superconductivity, and unexpected hysteresis in the magnetotransport. Our work demonstrates GeTe to be a potential host of topological SC and Majorana-like excitations, and to be a versatile platform to develop quantum information device architectures."
     return [doc]
 
@@ -84,10 +84,9 @@ def annotate(docs, model, model_type):
 
 def main(args):
     model_type = args.model_type
-    ammount = args.amt
 
     model = model_selection(model_type)
-    docs = get_documents(ammount)
+    docs = get_documents()
     tags = annotate(docs, model, model_type)
 
     print(tags)
