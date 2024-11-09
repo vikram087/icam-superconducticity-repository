a43929c21d144911f9b943a44f4923149aedab30
Ubuntu <ubuntu@ip-172-31-41-114.ec2.internal>
2024-11-09 22:23:15 +0000

    fixed typo with material-search

--- frontend/icam-materials-database/src/pages/material-search.jsx
+++ frontend/icam-materials-database/src/pages/material-search.jsx
@@ -160,7 +160,7 @@ function Table({ tableParams, setTableParams, setPrevUrl }) {
 			<NavBar />
 			<div className="mat-search-container">
 				<h1 style={{ marginTop: "-10px" }}>Search Materials</h1>
-				<Search searchParams={tableParams} to="/search-materials" />
+				<Search searchParams={tableParams} to="/material-search" />
 				<div className="content-container">
 					<div className="filters-section">
 						<Filters
